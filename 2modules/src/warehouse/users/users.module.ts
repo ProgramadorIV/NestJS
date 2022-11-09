b0250170bb4b91c70138a60c27733a0373af5290
ProgramadorIV <antoniojz1998@gmail.com>
2022-11-09 10:58:50 +0100

    trasteo con modulos.

---
+++ 2modules/src/warehouse/users/users.module.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+    imports: [],
+    controllers: [
+        UsersController
+    ],
+    providers: [
+        UsersService
+    ]
+})
+export class UsersModule {}
